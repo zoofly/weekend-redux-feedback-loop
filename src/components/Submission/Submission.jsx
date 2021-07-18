ccb38619059b7ecc12ecde283de33b0e4197a4c4
Kashia Moua <kserenitym@gmail.com>
2021-07-18 11:59:23 -0500

    base mode completed

--- src/components/Submission/Submission.jsx
+++ src/components/Submission/Submission.jsx
@@ -1,25 +1,21 @@
-import { useSelector } from 'react-redux';
 import React, {useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 
 function Submission() {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//         dispatch({
-//             type: 'ADD_FEELING_SCALE',
-//             payload: {feelingScale}
-//         });
-        
-//         history.push('/submission');
+  const history = useHistory();
 
-//     }
+  const handleSubmit = () => {
+    event.preventDefault();
+    history.push('/');
+
+    }
   return (
       <>
         <h1> Feedback! </h1>
 
         <div>
             <h2> Thank you! </h2>
-            <button type='submit'> Leave New Feedback </button>
+            <button type='submit' onClick= {handleSubmit}> Leave New Feedback </button>
             
         </div>
             
